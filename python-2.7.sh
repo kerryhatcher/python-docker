9955020572a3c53bdf13b2674457246683baf8cc
Kerry Hatcher <kwhatcher@gmail.com>
2015-12-16 10:25:23 -0500

    Update python-2.7.sh

--- python-2.7.sh
+++ python-2.7.sh
@@ -7,9 +7,9 @@ wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 
 tar -xvf Python-2.7.11.tgz
 
-sudo mkdir /opt/python2.7.11
+mkdir /opt/python2.7.11
 
-sudo chown -R vagrant:vagrant /opt/python2.7.11
+chown -R vagrant:vagrant /opt/python2.7.11
 
 cd Python-2.7.11
 
@@ -25,9 +25,9 @@ cd
 
 ### setup virtual python2.7.11 envroment
 
-sudo mkdir /opt/project_env2.7.11
+mkdir /opt/project_env2.7.11
 
-sudo chown vagrant:vagrant /opt/project_env2.7.11
+chown vagrant:vagrant /opt/project_env2.7.11
 
 /opt/python2.7.11/bin/python -m ensurepip --upgrade
 
@@ -39,13 +39,13 @@ sudo chown vagrant:vagrant /opt/project_env2.7.11
 
 source /opt/project_env2.7.11/bin/activate
 
-sudo /opt/project_env2.7.11/bin/pip2.7 install --upgrade pip
+/opt/project_env2.7.11/bin/pip2.7 install --upgrade pip
 
-sudo /opt/project_env2.7.11/bin/pip2.7 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -Upip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+/opt/project_env2.7.11/bin/pip2.7 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -Upip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 
-sudo /opt/project_env2.7.11/bin/pip2.7 install pyopenssl ndg-httpsclient pyasn1
+/opt/project_env2.7.11/bin/pip2.7 install pyopenssl ndg-httpsclient pyasn1
 
-sudo /opt/project_env2.7.11/bin/pip2.7 install -r /vagrant/requirements2.txt
+#/opt/project_env2.7.11/bin/pip2.7 install -r /vagrant/requirements2.txt
 
 
 deactivate
